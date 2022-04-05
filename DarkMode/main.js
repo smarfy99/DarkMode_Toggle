27f5e991a1cfcd4bc7c11e72d74096366a217f83
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-04-06 03:23:22 +0900

    ver2

--- DarkMode/main.js
+++ DarkMode/main.js
@@ -1,9 +1,26 @@
-const checkbox = document.getElementById('checkbox');
+// const checkbox = document.getElementById('checkbox');
+const checkbox = document.querySelector('#checkbox');
+console.log(checkbox);
 // addEventListener(event종류, 함수)
 // checkbox를 click 했을 때 해당 함수를 실행
-checkbox.addEventListener('click', () => {
-    //웹사이트의 배경 바꾸기
-    //toggle - 0과 1이 반복
-    //class가 존재하면 클래스 제거, 클래스가 존재하지 않으면 클래스 추가하는 메서드
-    document.body.classList.toggle('dark');
-});
+
+checkbox.addEventListener('click', click); 
+
+// 웹사이트의 배경 바꾸기
+// toggle - 0과 1이 반복 - 스위치처럼 on/off!
+// classList는 js가 css를 제어할 수 있게 함 - class가 존재하면 클래스 제거, 클래스가 존재하지 않으면 클래스 추가하는 메서드
+function click() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        console.log('convert into Light Mode');
+    } else {
+        document.body.classList.add('dark');
+        console.log('convert into Dark Mode');
+    }
+}
+
+
+// function toggleClick() {
+//     document.body.classList.toggle('dark');
+// }
+
